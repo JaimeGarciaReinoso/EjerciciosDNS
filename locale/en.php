3baa0bf7558f6abc693ce62403f58334d53c3f00
Jaime Garcia Reinoso <jaime.garciareinoso@uah.es>
2025-11-24 18:29:39 +0100

    Indice movido a index.php

---
+++ locale/en.php
@@ -0,0 +1,44 @@
+<?php
+
+$langArray = array(
+
+    "header" => "DNS Exercises", 
+    "notes" => "<h3>Important Notes:</h3>
+  <ol>
+    <li>This website is in a testing phase and may contain errors. If any error is detected, please send an email to <a href=\"mailto:jaime.garciareinoso@uah.es\">jaime.garciareinoso@uah.es</a></li>
+    <li>The fields requested in these exercises are a subset of those defined in the IP header and DNS messages.</li>
+    <li>It is recommended to do the exercise on paper first and then transfer it to the exercise form.</li>
+    <li>Once your answer is submitted, correct fields are marked in green and incorrect ones in red. If you have three or more errors, correction will stop and unchecked answers will be marked in yellow.</li>
+    <li>If you have any errors, it is recommended to click the \"back\" button in your browser and correct the incorrect fields.</li>
+    <li>Remember that you have the Augmented Mental Map of DNS <a href=\"https://mapamentalar1.web.uah.es/02-canvas/dns.html\">here</a> in case something is unclear.</li>
+    <li>The format of the messages requested in these exercises is unrelated to what might be asked in an AR1 exam.</li>
+  </ol>",
+ 
+   "authors" => "<h3>Authors</h3><ul>
+      <li>Jaime García Reinoso: original idea and basic functionality implementation.</li>
+      <li>Enrique de la Hoz de la Hoz: admin environment, improvement ideas and security.</li>
+      </ul>",
+
+  'exercise' => "Exercise",
+  'part' => "Part",
+  'checkOK' => "<h2>Congratulations! Everything is correct!</h2>",
+  "check1error" => "<h3>Sorry, there is one error. <br>Try it again by clicking the back button in your browser.</h3>",
+  'checkXerror1' => "<h3>Sorry, you have ",
+  'checkXerror2' => " error(s). <br>Try it again going back to the previous screen.</h3>",
+  'check3error' => "<h3>You have three or more errors, so your answer is not completely reviewed. <br>Try it again going back to the previous screen.</h3>",
+  'back' => "To go back to the main page click <a href=\"index.php\">here</a>",
+
+    "part" => "Part",
+    "exercise" => "Exercise",
+    "ex_no_congestion_title" => "Exercises WITHOUT congestion control",
+    "ex_1_title" => "Exercise 1 - Easy level",
+    "ex_2_title" => "Exercise 2 - Medium level",
+
+    // --- RESULTS ---
+    "correct_answer" => "Congratulations! Everything is correct!",
+    "one_error" => "Sorry, there is one error. <br>Try it again by clicking the back button in your browser.",
+    "n_errors" => "Sorry, you have {n} error(s). <br>Try it again going back to the previous screen.",
+    "many_errors" => "You have three or more errors, so your answer is not completely reviewed. <br>Try it again going back to the previous screen.",
+    "back_to_index" => "Back to index",
+);
+?>
